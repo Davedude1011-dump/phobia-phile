6dd86779f62e9b9d0c81a2d77883861c1cebcdbc
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-06-26 15:05:23 +0100

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,3 +1,4 @@
+var currentPage = "One"
 function common() {
     document.getElementById("common").style.display = "block"
     document.getElementById("rare").style.display = "none"
@@ -6,6 +7,8 @@ function common() {
     document.getElementById("boxBar1").style.backgroundColor = "white"
     document.getElementById("boxBar2").style.backgroundColor = "lightgrey"
     document.getElementById("boxBar3").style.backgroundColor = "lightgrey"
+
+    currentPage = "One"
 }
 
 function rare() {
@@ -16,6 +19,8 @@ function rare() {
     document.getElementById("boxBar1").style.backgroundColor = "lightgrey"
     document.getElementById("boxBar2").style.backgroundColor = "white"
     document.getElementById("boxBar3").style.backgroundColor = "lightgrey"
+
+    currentPage = "Two"
 }
 
 function strange() {
@@ -26,4 +31,41 @@ function strange() {
     document.getElementById("boxBar1").style.backgroundColor = "lightgrey"
     document.getElementById("boxBar2").style.backgroundColor = "lightgrey"
     document.getElementById("boxBar3").style.backgroundColor = "white"
+
+    currentPage = "Three"
+}
+
+function Box1Hover() {
+    document.getElementById("boxBar1").style.backgroundColor = "white"
+}
+function Box2Hover() {
+    document.getElementById("boxBar2").style.backgroundColor = "white"
+}
+function Box3Hover() {
+    document.getElementById("boxBar3").style.backgroundColor = "white"
+}
+
+function Box1NotHover() {
+    if (currentPage === "One") {
+        document.getElementById("boxBar1").style.backgroundColor = "white"
+    }
+    else {
+        document.getElementById("boxBar1").style.backgroundColor = "lightgrey"
+    }
+}
+function Box2NotHover() {
+    if (currentPage === "Two") {
+        document.getElementById("boxBar2").style.backgroundColor = "white"
+    }
+    else {
+        document.getElementById("boxBar2").style.backgroundColor = "lightgrey"
+    }
+}
+function Box3NotHover() {
+    if (currentPage === "Three") {
+        document.getElementById("boxBar3").style.backgroundColor = "white"
+    }
+    else {
+        document.getElementById("boxBar3").style.backgroundColor = "lightgrey"
+    }
 }
